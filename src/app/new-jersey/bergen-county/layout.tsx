078b153897288933e5742a9e1e61c2eb2367a9dc
Elmir's Mac mini <elmirmammadli18@gmail.com>
2026-09-20 21:57:56 -0400

    Add pointer cursors across NJ pages

--- src/app/new-jersey/bergen-county/layout.tsx
+++ src/app/new-jersey/bergen-county/layout.tsx
@@ -3,11 +3,11 @@ import BergenFooter from '@/components/BergenFooter';
 import BookingModal from '@/components/BookingModal';
 export default function BergenLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="[&_a]:cursor-pointer [&_button]:cursor-pointer [&_summary]:cursor-pointer">
       <Navbar />
       {children}
       <BookingModal />
       <BergenFooter />
-    </>
+    </div>
   );
 }
